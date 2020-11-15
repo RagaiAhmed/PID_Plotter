2d2b588bb179f6642a170980839aa8c773bb5628
Ragai <ragaiawad@gmail.com>
2020-11-15 15:48:03 +0200

    Simulating a sensor

---
+++ Helpers.py
@@ -0,0 +1,12 @@
+
+
+def sensor_read(x):
+    """
+        Acts as a feedback sensor
+    :return: "hypothetical" reading after applying x
+    """
+
+    return x  # Example of a perfect system
+
+
+
