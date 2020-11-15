8b66aaa4b9077f9a8a3bb267af069067f5cc8ea5
Ragai Ahmed <ragaiawad@gmail.com>
2020-11-15 17:46:41 +0200

    Adding starting condition

--- Helpers.py
+++ Helpers.py
@@ -5,6 +5,7 @@ def sensor_read(x):
         Acts as a feedback sensor
     :return: "hypothetical" reading after applying x
     """
+    if(x==-1): return 0
 
     return x  # Example of a perfect system
 
